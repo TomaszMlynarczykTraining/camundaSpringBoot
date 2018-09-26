a9493ba40d90b04a9fb0f1333b13f6c5c316973b
TomaszMlynarczykTraining <tomasz.feliks.mlynarczyk@gmail.com>
2018-09-26 13:43:54 +0200

    Create ApplicationMainConfiguration.java

---
+++ src/main/java/com/netia/iwork/Demo/ApplicationMainConfiguration.java
@@ -0,0 +1,9 @@
+package com.netia.iwork.Demo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan
+@Configuration
+public class ApplicationMainConfiguration {
+}
